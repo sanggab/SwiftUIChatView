630cb288f1085b89e237eb05bf2f1587555204a5
심상갑 <kappa2410@gmail.com>
2024-04-08 14:10:27 +0900

    Initial Commit

---
+++ SwiftUIChatView/SwiftUIChatViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatViewApp.swift
+//  SwiftUIChatView
+//
+//  Created by Gab on 2024/04/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
